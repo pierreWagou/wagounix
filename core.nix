45021179385681809ea60e7d206cef1725e4e0be
Pierre Romon <pierre.romon@gmail.com>
2026-03-01 20:22:31 +0100

    archi: restrucutre of the flake with packages and core instead of config

---
+++ core.nix
@@ -0,0 +1,36 @@
+{ config, lib, pkgs, catppuccin, ... }:
+
+{
+  imports = [
+    ./packages.nix
+    ./settings/control-center.nix
+    ./settings/dock.nix
+    ./settings/global-domain.nix
+    ./settings/finder.nix
+    ./settings/screen-capture.nix
+    ./settings/screen-saver.nix
+    ./settings/software-update.nix
+    ./settings/spaces.nix
+    ./settings/trackpad.nix
+  ];
+
+  nix = {
+    enable = false;
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      download-buffer-size = 524288000; # 500 MiB
+    };
+  };
+
+  system.stateVersion = 5;
+  system.primaryUser = "I544489";
+
+  security.pam.services.sudo_local.touchIdAuth = true;
+
+  users.users = {
+    I544489 = {
+      name = "I544489";
+      home = "/Users/I544489";
+    };
+  };
+}
