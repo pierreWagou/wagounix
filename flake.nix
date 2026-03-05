59ec4670d9fea0c32bfec30f73efef16ed38079a
Pierre Romon <pierre.romon@gmail.com>
2026-03-05 14:07:01 +0100

    chore: upgrade packages

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-25.11";
+      url = "github:NixOS/nixpkgs/nixos-unstable";
     };
     nix-darwin = {
-      url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
+      url = "github:nix-darwin/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     darwin-custom-icons = {
