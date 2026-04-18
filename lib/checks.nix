0a156ebff84486790daafabd9885eedabcd1844a
Pierre Romon <pierre.romon@gmail.com>
2026-04-18 11:59:35 +0200

    refactor: extract checks and develop shell

---
+++ lib/checks.nix
@@ -0,0 +1,40 @@
+{
+  self,
+  nixpkgs,
+  git-hooks,
+}:
+let
+  systems = [
+    "aarch64-darwin"
+    "x86_64-darwin"
+    "x86_64-linux"
+  ];
+in
+nixpkgs.lib.genAttrs systems (
+  system:
+  let
+    pre-commit-check = git-hooks.lib.${system}.run {
+      src = self;
+      hooks = {
+        nixfmt-rfc-style.enable = true;
+        statix.enable = true;
+        deadnix.enable = true;
+      };
+    };
+
+    darwinChecks = nixpkgs.lib.mapAttrs' (name: cfg: nixpkgs.lib.nameValuePair name cfg.system) (
+      nixpkgs.lib.filterAttrs (
+        _: cfg: cfg.pkgs.stdenv.hostPlatform.system == system
+      ) self.darwinConfigurations
+    );
+
+    nixosChecks =
+      nixpkgs.lib.mapAttrs' (name: cfg: nixpkgs.lib.nameValuePair name cfg.config.system.build.toplevel)
+        (
+          nixpkgs.lib.filterAttrs (
+            _: cfg: cfg.pkgs.stdenv.hostPlatform.system == system
+          ) self.nixosConfigurations
+        );
+  in
+  { inherit pre-commit-check; } // darwinChecks // nixosChecks
+)
---
+++ lib/devshell.nix
@@ -0,0 +1,24 @@
+{
+  self,
+  nixpkgs,
+}:
+let
+  systems = [
+    "aarch64-darwin"
+    "x86_64-darwin"
+    "x86_64-linux"
+  ];
+in
+nixpkgs.lib.genAttrs systems (
+  system:
+  let
+    pkgs = nixpkgs.legacyPackages.${system};
+    inherit (self.checks.${system}.pre-commit-check) shellHook enabledPackages;
+  in
+  {
+    default = pkgs.mkShell {
+      inherit shellHook;
+      buildInputs = enabledPackages;
+    };
+  }
+)
