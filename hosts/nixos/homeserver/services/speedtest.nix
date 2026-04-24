58939735135267aeeb563c922e99565849c9f8c7
Pierre Romon <pierre.romon@gmail.com>
2026-04-24 10:19:03 +0200

    feature: add speed test

--- hosts/nixos/homeserver/services/speedtest.nix
+++ hosts/nixos/homeserver/services/speedtest.nix
@@ -13,13 +13,15 @@
       PGID = "1000";
       TZ = "Europe/Paris";
       DB_CONNECTION = "sqlite";
+      DB_DATABASE = "/config/database.sqlite";
       SPEEDTEST_SCHEDULE = "0 3 * * *";
       APP_URL = "https://speed.${host.domain}";
     };
   };
 
-  # Ensure data directory exists
+  # Ensure data directory and SQLite database exist
   systemd.tmpfiles.rules = [
     "d /var/lib/speedtest-tracker 0755 1000 1000 -"
+    "f /var/lib/speedtest-tracker/database.sqlite 0644 1000 1000 -"
   ];
 }
