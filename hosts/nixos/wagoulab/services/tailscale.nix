a8960bab26bc906d51b4ee79eb11382edaf118f8
Pierre Romon <pierre.romon@gmail.com>
2026-04-29 00:41:58 +0200

    network: adding tailsacel for remote authentication to home network

--- hosts/nixos/wagoulab/services/tailscale.nix
+++ hosts/nixos/wagoulab/services/tailscale.nix
@@ -1,4 +1,6 @@
-_: {
+{ pkgs, ... }:
+
+{
   services.tailscale = {
     enable = true;
     openFirewall = true;
@@ -13,5 +15,19 @@ _: {
   boot.kernel.sysctl = {
     "net.ipv4.ip_forward" = 1;
     "net.ipv6.conf.all.forwarding" = 1;
+  };
+
+  # UDP GRO forwarding optimization for subnet router throughput
+  # See: https://tailscale.com/s/ethtool-config-udp-gro
+  systemd.services.tailscale-ethtool = {
+    description = "Configure UDP GRO forwarding for Tailscale subnet routing";
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.ethtool}/bin/ethtool -K enp170s0 rx-udp-gro-forwarding on rx-gro-list off";
+      RemainAfterExit = true;
+    };
   };
 }
