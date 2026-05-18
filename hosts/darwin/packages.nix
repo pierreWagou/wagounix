078c3172f32e457e1c8dd5e65f593242aefb8da1
Pierre Romon <pierre.romon@gmail.com>
2026-05-19 00:59:14 +0200

    feature: add ttyd for web connection to terminal

--- hosts/darwin/packages.nix
+++ hosts/darwin/packages.nix
@@ -1,46 +1,38 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
-  environment.systemPackages =
-    with pkgs;
-    [
-      # Darwin-specific
-      cocoapods
-      darwin.trash
-      git-credential-manager
-      pinentry_mac
-      spicetify-cli
-      spotify-player
+  environment.systemPackages = with pkgs; [
+    # Darwin-specific
+    cocoapods
+    darwin.trash
+    git-credential-manager
+    pinentry_mac
+    spicetify-cli
+    spotify-player
 
-      # Workstation tools (not needed on the homeserver)
-      copier
-      delta
-      ffmpeg
-      fnm
-      git-lfs
-      imagemagick
-      lazygit
-      maven
-      mise
-      mprocs
-      mutt-wizard
-      nix-index
-      openapi-generator-cli
-      openvpn
-      pay-respects
-      poetry
-      qrencode
-      R
-      scala
-      television
-      terraform
-      tree-sitter
-      uv
-      vivid
-      worktrunk
-      yt-dlp
-    ]
-    ++ lib.optionals (pkgs.stdenv.hostPlatform.system != "x86_64-darwin") [
-      opencode
-    ];
+    # Workstation tools (not needed on the homeserver)
+    copier
+    delta
+    ffmpeg
+    fnm
+    git-lfs
+    imagemagick
+    lazygit
+    maven
+    mutt-wizard
+    nix-index
+    openapi-generator-cli
+    openvpn
+    pay-respects
+    poetry
+    qrencode
+    R
+    scala
+    terraform
+    tree-sitter
+    uv
+    vivid
+    worktrunk
+    yt-dlp
+  ];
 }
