17f485377a650709892b7705e57fbe0827fc9ded
Pierre Romon <pierre.romon@gmail.com>
2026-05-28 16:28:55 +0200

    refactor: branding become a module

--- hosts/nixos/wagoulab/services/branding.nix
+++ hosts/nixos/wagoulab/services/branding.nix
@@ -73,8 +73,14 @@ let
   # === Logo Generator ===
   mkLogo =
     name:
+    let
+      textContent = "WAGOU ${name}";
+      textLen = builtins.stringLength textContent;
+      textWidth = textLen * 12 + 16;
+      totalWidth = 38 + textWidth;
+    in
     pkgs.writeText "logo-${lib.toLower name}.svg" ''
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 40" preserveAspectRatio="xMinYMid meet">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${toString totalWidth} 40" preserveAspectRatio="xMinYMid meet">
         <defs>
           <linearGradient id="sun" x1="0" y1="0" x2="0" y2="1">
             <stop offset="0%" stop-color="${gradient.start}"/>
