5d9704c98e33d375edd92c785b45cce2b1420a85
Pierre Romon <pierre.romon@gmail.com>
2026-06-06 04:00:48 +0200

    cosmetic: add slcak and try to change theme

---
+++ hosts/darwin/settings/appearance.nix
@@ -0,0 +1,4 @@
+_: {
+  system.defaults.CustomUserPreferences.NSGlobalDomain.AppleAccentColor = 5; # Purple
+  system.defaults.CustomUserPreferences."com.apple.finder".FXPreferredTagColor = 5; # Purple folders
+}
