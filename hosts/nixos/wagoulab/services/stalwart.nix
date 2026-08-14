3bd4551db12a4ddd1884059f402789bd02c0fd80
Pierre Romon <pierre.romon@gmail.com>
2026-08-14 16:41:11 +0200

    stalwart: remove recovery mode - initial setup complete

--- hosts/nixos/wagoulab/services/stalwart.nix
+++ hosts/nixos/wagoulab/services/stalwart.nix
@@ -26,7 +26,6 @@ in
       environments = {
         STALWART_HOSTNAME = host.domain;
         STALWART_PUBLIC_URL = "https://mailbox.${host.domain}";
-        STALWART_RECOVERY_MODE = "1";
       };
       labels = {
         "traefik.enable" = "true";
