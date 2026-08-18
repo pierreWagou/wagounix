ab4752a8066305d4e65242e06d59156f9237f012
Pierre Romon <pierre.romon@gmail.com>
2026-08-18 21:42:55 +0200

    stalwart: enable recovery mode temporarily for LDAP filter fix

--- hosts/nixos/wagoulab/services/stalwart.nix
+++ hosts/nixos/wagoulab/services/stalwart.nix
@@ -27,6 +27,7 @@ in
       environments = {
         STALWART_HOSTNAME = "mail.${host.domain}";
         STALWART_PUBLIC_URL = "https://mailbox.${host.domain}";
+        STALWART_RECOVERY_MODE = "1";
       };
       labels = {
         "traefik.enable" = "true";
